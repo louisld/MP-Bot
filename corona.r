a142e9399469de28f75b8a4c7913b3ab84e58d18
louisld <louisldthesamui@gmail.com>
2020-03-28 13:36:32 +0100

    Amélioration des statistiques et renommage des variables en accordance avec la source

--- corona.r
+++ corona.r
@@ -1,49 +1,71 @@
 #!/usr/bin/Rscript
 
-#these libraries are necessary
-
 library(readxl)
-
 library(httr)
 
-#create the URL where the dataset is stored with automatic updates every day
-
-#url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-23.xlsx")
-
+#Récupération du fichier excel
+#url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide.xlsx")
 url <- paste("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-",format(Sys.time(), "%Y-%m-%d"), ".xlsx", sep = "")
 
-#download the dataset from the website to a local temporary file
-
 GET(url, authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".xlsx")))
-
-#read the Dataset sheet into “R”
-
 data <- read_excel(tf)
-names(data)[names(data) == 'Countries and territories'] <- 'countries'
+names(data)[names(data) == 'countriesAndTerritories'] <- 'countries'
 
 #Sélection des données
-dataF <- data[data$countries == "France" & (data$Month >= 3 | (data$Day >= 27 & data$Month >= 2)) & (data$Month != 12 & data$Day != 31 & data$Year != 2019), ]
+dataF <- data[data$countries == "France" & (data$month >= 3 | (data$day >= 27 & data$month >= 2)) & (data$month != 12 & data$day != 31 & data$year != 2019), ]
 
-#Régression linéaire
+#Régression linéaire pour les nouveaux cas
 dataT <- dataF
+zeroDay <- dataT$dateRep[nrow(dataT)]
+dataT <- transform(dataT, dateRep = as.numeric(difftime(dateRep, zeroDay), units="days"))
 dataT[,c(5)] <- log(dataT[,c(5)])
-zeroDay <- dataT$DateRep[nrow(dataT)]
-dataT <- transform(dataT, DateRep = as.numeric(difftime(DateRep, zeroDay), units="days"))
-regression <- lm(dataT$Cases ~ dataT$DateRep)
+regression <- lm(dataT$cases ~ dataT$dateRep)
+
+#Régression linéaire pour le nombre de morts
+dataD <- dataT[dataT$day >= 6 & dataT$month >= 3,]
+dataDT <- dataD
+dataDT[,c(6)] <- log(dataDT[,c(6)])
+regressionDeaths <- lm(dataDT$deaths ~ dataDT$dateRep)
+
+#Coefficient de detérmination
+r = summary(regression)$r.squared
+rD = summary(regressionDeaths)$r.squared
 
 #Graphique
 png("result.png", width=480, height=480)
-plot(dataT$DateRep,dataF$Cases, main="Cas de Coronavirus en France", xlab="Jours depuis le 27 février", ylab="Cas")
+plot(dataT$dateRep,dataF$cases, main="Nouveaux cas de Coronavirus en France", xlab="Jours depuis le 27 février", ylab="Cas", col="orange")
+points(dataD$dateRep, dataD$deaths, col="red")
 courbe <- function(x) exp(coef(regression)[2]*x+coef(regression)[1])
+courbeDeaths <- function(x) exp(coef(regressionDeaths)[2]*x+coef(regressionDeaths)[1])
 dataT[,c(5)] <- courbe(dataT[,c(1)])
-lines(dataT$DateRep, dataT$Cases, col="red")
+dataDT[,c(6)] <- courbeDeaths(dataDT[,c(1)])
+lines(dataT$dateRep, dataT$cases, col="orange")
+lines(dataD$dateRep, dataDT$deaths, col="red")
+text(paste("r²(nouveaux cas) = ", round(r, digits=3)), x=0, y=3500, pos=4)
+text(paste("r²(morts) = ", round(rD, digits=3)), x=0, y=3000, pos=4)
 dev.off()
 
-end <- (log(68000000 - coef(regression)[1])/coef(regression)[2])-dataT$DateRep[1]
+#Projections ssur la France
+end <- (log(68000000) - coef(regression)[1])/coef(regression)[2]-dataT$dateRep[1]
+endDeaths <- (log(68000000) - coef(regressionDeaths)[1])/coef(regressionDeaths)[2]-dataT$dateRep[1]
 
+#Nombre total depuis le début
+total <- sum(dataF$cases)
+totalD <- sum(dataF$deaths)
+
+#Renommage des variables pour le JSON
 names(end) <- "end"
+names(endDeaths) <- "endDeaths"
+names(r) <- "rsquared"
+names(rD) <- "rsquaredDeaths"
+names(total) <- "total"
+names(totalD) <- "totalD"
 a <- coef(regression)[2]
 b <- coef(regression)[1]
+aD <- coef(regressionDeaths)[2]
+bD <- coef(regressionDeaths)[1]
 names(a) <- "a"
 names(b) <- "b"
-result <- c(dataF, a, b, end)
+names(aD) <- "aD"
+names(bD) <- "bD"
+result <- c(dataF, a, b, aD, bD, end, endDeaths, r, rD, total, totalD)
--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const config = require('./config.json');
 const edt = require('./edt.json');
 const client = new Discord.Client();
 
+//Déclaration des variables contenant les ids
 var channelTest;
 var channelInformation;
 
+//Tables pour afficher la date
 var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
 var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
 
+//Initialisation du bot
 client.login(config.token);
-
 client.once("ready", () => {
   console.log("Je suis prêt !");
   client.user.setActivity("Intégrales généralisées");
@@ -21,51 +23,71 @@ client.once("ready", () => {
   channelTest = client.channels.cache.get(config.channelTest);
 });
 
+//Réaction aux messages des utilisateurs
 client.on('message', message => {
   if (message.content === `${config.prefix}ping`) {
     message.channel.send('pong');
   }
 });
 
+//Message quotidien avec texte personnalisé et météo à 7h30
 var j = schedule.scheduleJob('30 7 * * *', function(){
   openweathermap("Paris, France", function(success, previsions){
     var today = new Date();
     message = "";
     try{
+      if(!success) throw success;
       message = edt.days[today.getDate()-22].text
     }catch(e){
-      channelTest.send("Pas de message personnalisé aujourd'hui :( : " + e);
+      channelTest.send("Pas de message personnalisé aujourd'hui :( : " + e + "/météo :"+ success);
     }
     channelInformation.send("Bonjour,\nnous sommes le "+jours[today.getDay()]+" "+today.getDate()+" "+mois[today.getMonth()]+" "+today.getFullYear()+". Le temps sera "+previsions.condition+" et la température sera de "+previsions.temperature+"°C.\n"+edt.days[today.getDate()-22].text+"\nBonne journée.");
   });
 });
 
-var j = schedule.scheduleJob('0 18 * * *', function(){
+//Statistique quotidienne sur le coronavirus (voir corona.r) à 16h00
+var j = schedule.scheduleJob('0 16 * * *', function(){
   R("corona.r").call(function(err, data) {
-      if (err) throw err;
-      const message = new Discord.MessageEmbed()
-        .setTitle("Statistiques coronavirus")
-        .setFooter("Source : ©Chauve Capé Corp")
-        .attachFiles(['result.png'])
-        .setImage("attachment://result.png")
-        .addFields(
-          {name: "Nouveaux cas", value: data.Cases[0]},
-          {name: "Nombre de morts", value: data.Deaths[0]},
-          {name: "Modélisation des nouveaux cas (j(0)=27 février): ", value: "N(j)=exp("+data.a+"j+"+data.b+")"},
-          {name: "Tous les français infecté dans :", value: Math.ceil(data.end)+" jours"}
-        );
-      channelInformation.send(message);
+      if (err){
+        channelTest.send("Erreur d'exécution du script R");
+      } else {
+        const message = new Discord.MessageEmbed()
+          .setTitle("Statistiques coronavirus")
+          .setFooter("Source : ©Chauve Capé Corp")
+          .attachFiles(['result.png'])
+          .setImage("attachment://result.png")
+          .setTimestamp()
+          .addFields(
+            {name: "Nouveaux cas", value: data.cases[0], inline: true},
+            {name: '\u200B', value: '\u200B', inline: true},
+            {name: "Nouveaux morts", value: data.deaths[0], inline: true},
+            {name: "Cas totaux", value: data.total, inline: true},
+            {name: '\u200B', value: '\u200B', inline: true},
+            {name: "Morts totaux", value: data.totalD, inline: true},
+            {name: "Tous les français infectés dans :", value: Math.ceil(data.end)+" jours"},
+            {name: "Tous les français morts dans :", value: Math.ceil(data.endDeaths)+" jours"},
+            {name: "Modélisation des nouveaux cas (j(0)=27 février): ", value: "N(j)=exp("+data.a+"j+"+data.b+")"},
+            {name: "Modélisation du nombre de morts (j(0)=27 février): ", value: "N(j)=exp("+data.aD+"j"+data.bD+")"},
+          );
+        channelInformation.send(message);
+      }
     });
 });
 
+//Annonce de la pause de 10h30
 var pause1 = schedule.scheduleJob('30 10 * * *', function(){
-  channelInformation.send("Une pause s'impose");
+  var today = new Date();
+  if(today.getDay() != 5 && today.getDay() != 6)
+    channelInformation.send("Une pause s'impose");
 });
 
+//Annonce du repas de midi à 12h30
 var pause2 = schedule.scheduleJob('30 12 * * *', function(){
-  channelInformation.send("J'ai cru entendre des ventres gargouiller. Il est temps de reprendre des forces.");
+  if(today.getDay() != 5 && today.getDay() != 6)
+    channelInformation.send("J'ai cru entendre des ventres gargouiller. Il est temps de reprendre des forces.");
 });
 
+//Fonction de récupération des données météo depuis l'api openweathermap
 var openweathermap = function(city, callback){
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&lang=fr&units=metric&appid=' + config.openWeatherMapId;
 
